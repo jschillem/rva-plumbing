99e298ab694e425e4ce18fddea9bf9f10738dbc6
Justin Schilleman <justin@schilleman.com>
2023-10-11 23:22:39 -0400

    made text centered for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,8 @@
 </svelte:head>
 
 <div class="flex items-center flex-col gap-3">
-	<h1 class="text-slate-800 text-6xl font-bold">Stephen’s Plumbing and Gas LLC</h1>
-	<h2 class="text-2xl font-semibold text-slate-800">Guaranteed Craftsmanship!</h2>
+	<h1 class="text-center text-slate-800 text-6xl font-bold">Stephen’s Plumbing and Gas LLC</h1>
+	<h2 class="text-center text-2xl font-semibold text-slate-800">Guaranteed Craftsmanship!</h2>
 	<h3 class="text-xl">(804) 914-7711</h3>
 	<a
 		class="font-semibold text-slate-100 bg-slate-800 hover:bg-slate-500 hover:scale-110 px-5 py-3 m-4 ease-in-out duration-100"
